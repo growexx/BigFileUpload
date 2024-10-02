92bb8834a29be7cfbbdc4106a6d5692291e259c6
Hitesh Parikh <hitesh.parikh@growexx.com>
2024-10-02 10:23:35 +0530

    Add error message to upload

--- server/services/fileUpload/fileUploadController.js
+++ server/services/fileUpload/fileUploadController.js
@@ -7,6 +7,7 @@ const fileUploadController = {
             const data = await fileUploadService.initiateUpload(bucketName, key);
             res.status(200).json(data);
         } catch (error) {
+            console.log("initiateUpload Error", error);
             res.status(500).json({ error: error.message });
         }
     },
@@ -17,6 +18,7 @@ const fileUploadController = {
             const presignedUrls = await fileUploadService.generatePresignedUrls(bucketName, key, uploadId, partNumbers);
             res.status(200).json({ presignedUrls });
         } catch (error) {
+            console.log("generatePresignedUrls Error", error);
             res.status(500).json({ error: error.message });
         }
     },
@@ -26,6 +28,7 @@ const fileUploadController = {
             const data = await fileUploadService.completeMultipartUpload(bucketName, key, uploadId, parts);
             res.status(200).json(data);
         } catch (error) {
+            console.log("completeUpload Error", error);
             res.status(500).json({ error: error.message });
         }
     }
